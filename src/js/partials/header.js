75bf372d155a308493db588082b3d1edffdece1d
burdms <burdms@gmail.com>
2021-12-03 21:22:47 +0100

    create: cover responsive

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -2,7 +2,6 @@ function header() {
   const header = document.querySelector('.header');
 
   header.addEventListener('click', e => {
-    console.log(e.target);
     if (e.target.closest('.header-langs')) {
       document.querySelector('.header-langs').classList.toggle('opened');
     }
@@ -11,6 +10,14 @@ function header() {
       header.querySelector('.header-menu').classList.toggle('active');
       header.querySelector('.header-burger').classList.toggle('active');
       document.body.classList.toggle('no-scroll');
+    }
+
+    if (window.matchMedia("(max-width: 1079px)").matches) {
+      if (e.target.classList.contains('link')) {
+        header.querySelector('.header-menu').classList.toggle('active');
+        header.querySelector('.header-burger').classList.toggle('active');
+        document.body.classList.toggle('no-scroll');
+      }
     }
   });
 }
