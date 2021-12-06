68cccdaac6044a4adfed7c13943d038426d86c0a
burdms <burdms@gmail.com>
2021-12-06 12:51:57 +0100

    create: team responsive

--- src/js/partials/teamSlider.js
+++ src/js/partials/teamSlider.js
@@ -1,10 +1,27 @@
 function teamSlider() {
   const teamSlider = new Swiper(".team-slider", {
-    spaceBetween: 30,
-    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 3000,
+      stopOnLastSlide: true,
+    },
     navigation: {
       prevEl: ".team-slider__button--prev",
       nextEl: ".team-slider__button--next",
+    },
+    breakpoints: {
+      421: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+        autoplay: false,
+      },
+      1079: {
+        spaceBetween: 30,
+        slidesPerView: 4,
+      },
     },
   });
 }
